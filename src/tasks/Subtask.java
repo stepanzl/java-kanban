54f43dc939b0da7a2d134ca63f4c9c539e72de42
stepanzl <stepan.zalizniy@yandex.ru>
2025-04-13 23:42:39 +0300

    fix CheckStyle

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -8,10 +8,12 @@ public class Subtask extends Task {
         super(name, description, status);
         this.epicId = epicId;
     }
+
     public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
         super(id, name, description, status);
         this.epicId = epicId;
     }
+
     public Subtask(Subtask other) {
         super(other);
         this.epicId = other.epicId;
